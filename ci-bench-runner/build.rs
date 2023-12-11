3c513e6d26ac529cd71d243450b187ea515e1a17
Adolfo Ochagavía <github@adolfo.ochagavia.nl>
2023-12-11 16:18:33 +0100

    fix: replace `vergen` by directly running git
    For some reason, `vergen` was unable to find the git binary when
    building inside Ansible. Running git directly fixes the problem... and
    we also have one dependency less! 🎉

--- ci-bench-runner/build.rs
+++ ci-bench-runner/build.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // In some cases if new migrations are added without accompanying Rust src changes
     // `cargo build` isn't smart enough to detect the need for recompilation to pick up
@@ -5,7 +7,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // See <https://docs.rs/sqlx/latest/sqlx/macro.migrate.html#triggering-recompilation-on-migration-changes>
     println!("cargo:rerun-if-changed=migrations");
 
-    // Expose git-related information through environment variables at compile-time
-    vergen::EmitBuilder::builder().all_git().emit()?;
+    // Trigger recompilation if the repository's head changed, to make sure the commit information
+    // embedded in the binary is up-to-date
+    println!("cargo:rerun-if-changed=.git/HEAD");
+
+    // Expose git head information through environment variables at compile-time, so it can be
+    // embedded in the binary through the `env!` macro
+    let output = Command::new("git").args(["rev-parse", "HEAD"]).output()?;
+    let git_sha = String::from_utf8(output.stdout)?;
+    println!("cargo:rustc-env=GIT_HEAD_SHA={}", git_sha);
+
+    let output = Command::new("git")
+        .args(["show-branch", "--no-name", "HEAD"])
+        .output()?;
+    let git_commit_message = String::from_utf8(output.stdout)?;
+    println!("cargo:rustc-env=GIT_HEAD_COMMIT_MESSAGE={git_commit_message}");
+
     Ok(())
 }
